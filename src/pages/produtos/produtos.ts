4443b3cef408ffb126a9ea1e797b300f60392c00
Bruno <brunosuportefs@gmail.com>
2019-04-18 22:43:37 -0300

    carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -22,9 +23,22 @@ export class ProdutosPage {
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id).subscribe(
       response => {
-        this.items = response['content'];//pega só o conteúdo do content        
+        this.items = response['content'];//pega só o conteúdo do content    
+        this.loadImageUrls();    
       },
       error => {}
     );  
+  }
+
+  loadImageUrls(){
+    for (var i=0; i<this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id).subscribe(
+        response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {}
+      );
+    }
   }
 }
